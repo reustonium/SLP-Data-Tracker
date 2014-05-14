0a5ed0b8c7e7fa993c9736b607d084d4a63053f4
reustonium <andrew.ruestow@gmail.com>
2014-05-14 17:15:21 -0400

    basic calculator functionality

--- slpdatatracker/src/main/java/com/reustonium/slpdatatracker/MainActivity.java
+++ slpdatatracker/src/main/java/com/reustonium/slpdatatracker/MainActivity.java
@@ -1,19 +1,13 @@
 package com.reustonium.slpdatatracker;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 
 
-
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- slpdatatracker/src/main/java/com/reustonium/slpdatatracker/TrackerFragment.java
+++ slpdatatracker/src/main/java/com/reustonium/slpdatatracker/TrackerFragment.java
@@ -1,15 +1,27 @@
 package com.reustonium.slpdatatracker;
 
-import android.app.Activity;
-import android.net.Uri;
 import android.os.Bundle;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
+import android.widget.TextView;
 
 public class TrackerFragment extends Fragment {
+
+    int numQuestions;
+    int numCorrect;
+    int numCue;
+
+    TextView tv_success;
+    TextView tv_successData;
+    TextView tv_cue;
+    TextView tv_cueData;
+
+    Button btn_correct;
+    Button btn_cue;
+    Button btn_wrong;
 
     public TrackerFragment(){
 
@@ -19,6 +31,70 @@ public class TrackerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_tracker, container, false);
+
+        numQuestions = 0;
+        numCorrect = 0;
+        numCue = 0;
+
+        tv_success = (TextView)rootView.findViewById(R.id.tracker_txt_success);
+        tv_successData = (TextView)rootView.findViewById(R.id.tracker_txt_success_data);
+        tv_cue = (TextView)rootView.findViewById(R.id.tracker_txt_cue);
+        tv_cueData = (TextView)rootView.findViewById(R.id.tracker_txt_cue_data);
+
+        updateTextViews();
+
+        btn_correct = (Button)rootView.findViewById(R.id.tracker_btn_correct);
+        btn_cue = (Button)rootView.findViewById(R.id.tracker_btn_cue);
+        btn_wrong = (Button)rootView.findViewById(R.id.tracker_btn_wrong);
+
+        btn_correct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numQuestions++;
+                numCorrect++;
+                updateTextViews();
+            }
+        });
+
+        btn_cue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numQuestions++;
+                numCue++;
+                updateTextViews();
+            }
+        });
+
+        btn_wrong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numQuestions++;
+                updateTextViews();
+            }
+        });
+
         return rootView;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+
+    }
+
+    public void updateTextViews(){
+        if(numQuestions == 0){
+            tv_success.setText("0%");
+            tv_successData.setText("0/0");
+            tv_cue.setText("0%");
+            tv_cueData.setText("0/0");
+        } else {
+
+            tv_success.setText(String.format("%d%%", Math.round(numCorrect * 100/numQuestions)));
+            tv_successData.setText(String.format("%d / %d", numCorrect, numQuestions));
+            tv_cue.setText(String.format("%d%%", Math.round((numCorrect+numCue)* 100 /numQuestions)));
+            tv_cueData.setText(String.format("%d / %d", (numCorrect+numCue), numQuestions));
+        }
     }
 }
